b2884a46b3f0512ad3d29fef88b45c85f4e29a80
Vanessa Barbosa dos Santos <vanessabsantoseng@gmail.com>
2024-09-07 17:17:35 -0300

    projeto inicial

--- main.js
+++ main.js
@@ -6,18 +6,22 @@ let guess; //variavel de chute
 let guess_count = 1; //contagem de chute
 
 while (secret_number != guess){  //loop de tentativas
-    guess = prompt(`Type a number between 0 and ${limit}:`);
-    
-    if (secret_number == guess) { //condição se acertar
-        let word_attempts = guess_count > 1 ? 'attempts' : 'attempt'; //substituto para if else
-        alert(`Well done, you win in ${guess_count} ${word_attempts} S2!`);
-    } else { //condição se errar - dicas de chute
-        alert("Tray again :(");
-        if(secret_number < guess){
-            alert(`The secret number is less than your guees ${guess} ;)`);
-        } else {
-            alert(`The secret number is greater than your guees ${guess} ;)`);
+    guess = prompt(`Type a number between 1 and ${limit}:`);
+
+    if (isNaN(guess) || guess < 1 || guess > limit) { //verifica se o que foi digitado é um numero e se esta dentro do intervalo de 1 a 100
+        alert(`Plase type a valid number between 1 and ${limit}`);
+    } else {
+        if (secret_number == guess) { //condição se acertar
+            let word_attempts = guess_count > 1 ? 'attempts' : 'attempt'; //substituto para if else
+            alert(`Well done, you win in ${guess_count} ${word_attempts} S2!`);
+        } else { //condição se errar - dicas de chute
+            alert("Tray again :(");
+            if(secret_number < guess){
+                alert(`The secret number is less than your guees ${guess} ;)`);
+            } else {
+                alert(`The secret number is greater than your guees ${guess} ;)`);
+            }
+            guess_count++;
         }
-        guess_count++;
     }
 }
